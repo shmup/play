792324ef3f2daa6dde33e3d5d31d60a8a04e4dd5
Jared Miller <jared@smell.flowers>
2025-04-18 11:13:16 -0400

    Display chat on client connection

--- src/plugins/chat/server.ts
+++ src/plugins/chat/server.ts
@@ -11,6 +11,19 @@ export const ChatServerPlugin: ServerPlugin = {
       (state as any).chat = [];
     });
   },
+  onClientConnect(clientId: string, context: ServerPluginContext) {
+    const state = context.getState() as any;
+    const chats = state.chat as { clientId: string; text: string }[] | undefined;
+    if (chats && chats.length > 0) {
+      for (const chatData of chats) {
+        context.sendTo(clientId, {
+          type: "custom",
+          pluginId: PLUGIN_ID,
+          data: chatData,
+        });
+      }
+    }
+  },
 
   onMessage(
     clientId: string,
@@ -20,6 +33,9 @@ export const ChatServerPlugin: ServerPlugin = {
     if (message.type === "custom" && (message as any).pluginId === PLUGIN_ID) {
       const data = (message as any).data as { text: string };
       const chatData = { clientId, text: data.text };
+      context.setState((state) => {
+        (state as any).chat.push(chatData);
+      });
       context.broadcast({
         type: "custom",
         pluginId: PLUGIN_ID,
