d9a9289cb840445708077fc1a187c523cc32de58
Jared Miller <jared@smell.flowers>
2025-04-19 15:58:27 -0400

    Wire in custom contet menu to client

--- src/client.ts
+++ src/client.ts
@@ -5,10 +5,29 @@ import {
 import { ChatPlugin } from "./plugins/chat/index.ts";
 import { CursorPlugin } from "./plugins/cursors/index.ts";
 import { DrawPlugin } from "./plugins/draw/index.ts";
-import { ContextMenuPlugin } from "./plugins/contextmenu/index.ts";
+import { createContextMenuPlugin } from "./plugins/contextmenu/index.ts";
 
 registerPlugin(ChatPlugin);
 registerPlugin(CursorPlugin);
 registerPlugin(DrawPlugin);
-registerPlugin(ContextMenuPlugin);
+// Context menu plugin with custom actions
+registerPlugin(
+  createContextMenuPlugin([
+    {
+      label: "Clear Canvas",
+      value: "clearCanvas",
+      onClick: (context) => {
+        // send a custom message; handle on server if desired
+        context.sendMessage({ type: "custom", pluginId: "contextmenu", data: { action: "clearCanvas" } });
+      },
+    },
+    {
+      label: "Log State",
+      value: "logState",
+      onClick: (context) => {
+        console.log("App state:", context.getState());
+      },
+    },
+  ])
+);
 initializeClient();
